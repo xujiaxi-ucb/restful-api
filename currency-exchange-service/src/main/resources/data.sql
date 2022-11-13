e6a63580da04425946ab411604b1905d3049d064
Jiaxi Xu <xujiaxi@berkeley.edu>
2022-11-13 00:28:13 -0600

    API finished

---
+++ currency-exchange-service/src/main/resources/data.sql
@@ -0,0 +1,9 @@
+insert into currency_exchange
+(id,currency_from,currency_to,conversion_multiple,environment)
+values(10001,'USD','CNY',7.11,'');
+insert into currency_exchange
+(id,currency_from,currency_to,conversion_multiple,environment)
+values(10002,'EUR','CNY',7.37,'');
+insert into currency_exchange
+(id,currency_from,currency_to,conversion_multiple,environment)
+values(10003,'AUD','CNY',4.77,'');
